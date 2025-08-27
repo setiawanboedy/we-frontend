9fb42fc4617c13c2a998d5062d52ebb6e222a972
setiawanboedy <gonisetiawan0@gmail.com>
2025-08-27 23:32:37 +0800

    feat: init dependency injection in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { initializeServices } from './di/InjectionRegistry'
+
+initializeServices()
 
 const app = createApp(App)
 
