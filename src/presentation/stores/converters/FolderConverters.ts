be0cc2f58bf01baad268edae3d6b610781147fde
setiawanboedy <gonisetiawan0@gmail.com>
2025-08-27 23:48:35 +0800

    feat: add convert data node to ui or othersn

---
+++ src/presentation/stores/converters/FolderConverters.ts
@@ -0,0 +1,33 @@
+import type { FolderWithChildrenDto } from "@/application/dto/FolderDto"
+import  { FolderHierarchy, FolderNode } from "@/domain/entities/FolderHierarchy"
+import type { FolderItem } from "@/shared/types/explorer"
+
+export class FolderConverters {
+     static convertDtoToDomainHierarchy(dtos: FolderWithChildrenDto[]): FolderHierarchy {
+    const nodes = dtos.map((dto) => this.convertDtoToDomainNode(dto))
+    return new FolderHierarchy(nodes)
+  }
+
+  static convertDtoToDomainNode(dto: FolderWithChildrenDto): FolderNode {
+    const children = dto.children.map((child) => this.convertDtoToDomainNode(child))
+    return new FolderNode(dto.id, dto.name, dto.path, dto.parentId, dto.size, children)
+  }
+
+  static convertFolderNodeToUI(node: FolderNode): FolderItem {
+    return {
+      id: node.id,
+      name: node.name,
+      size: node.size ?? '',
+      icon: 'folder',
+      path: node.path,
+      updateAt: '', 
+      parentId: node.parentId,
+      isExpanded: false,
+      children: node.children.map((child) => this.convertFolderNodeToUI(child)),
+    }
+  }
+
+  static convertFolderHierarchyToUI(hierarchy: FolderHierarchy): FolderItem[] {
+    return hierarchy.folders.map((node) => this.convertFolderNodeToUI(node))
+  }
+}
