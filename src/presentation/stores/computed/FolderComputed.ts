183c44215d4e63c365ea7a2dc15874e7b2fde923
setiawanboedy <gonisetiawan0@gmail.com>
2025-08-27 23:47:22 +0800

    feat: add computed handler action

---
+++ src/presentation/stores/computed/FolderComputed.ts
@@ -0,0 +1,33 @@
+import { computed } from "vue"
+import type { FolderState } from "../state/FolderState"
+import { FolderConverters } from "../converters/FolderConverters"
+
+export class FolderComputed {
+    constructor(private state: FolderState) {}
+
+  readonly selectedFolder = computed(() => {
+    if (!this.state.selectedFolderId.value || !this.state.folderHierarchy.value) return null
+    const folderNode = this.state.folderHierarchy.value.findById(this.state.selectedFolderId.value)
+    return folderNode ? FolderConverters.convertFolderNodeToUI(folderNode) : null
+  })
+
+  readonly breadcrumbPath = computed(() => {
+    if (!this.selectedFolder.value) return ['This PC']
+    return [...this.state.currentPath.value.split(' > ').filter(Boolean)]
+  })
+
+  readonly displayedFolders = computed(() => {
+    if (this.state.isSearchMode.value) {
+      return this.state.searchResults.value
+    }
+    return this.state.currentFolderChildren.value
+  })
+
+  readonly canGoBack = computed(() => {
+    return this.state.currentHistoryIndex.value > 0
+  })
+
+  readonly canGoForward = computed(() => {
+    return this.state.currentHistoryIndex.value < this.state.navigationHistory.value.length - 1
+  })
+}
