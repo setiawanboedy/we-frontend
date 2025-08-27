b199098129a68400ddfa6e2b729c52fa19217566
setiawanboedy <gonisetiawan0@gmail.com>
2025-08-27 23:58:57 +0800

    feat: update add nav forward and nav back

--- src/presentation/stores/actions/FolderSelectionActions.ts
+++ src/presentation/stores/actions/FolderSelectionActions.ts
@@ -1,15 +1,20 @@
-import type { IFolderDataService } from '@/domain/interfaces/IFolderServices'
+import type { IFolderDataService, INavigationHistoryService } from '@/domain/interfaces/IFolderServices'
 import type { FolderState } from '../state/FolderState'
 
 export class FolderSelectionActions {
   constructor(
     private state: FolderState,
     private loadingActions: IFolderDataService,
+    private navigationService: INavigationHistoryService,
   ) {}
 
   async selectFolder(folderId: string, skipHistory = false): Promise<void> {
     if (this.state.isSearchMode.value) {
       this.clearSearch()
+    }
+
+    if (!skipHistory) {
+      this.addToHistory(folderId)
     }
 
     this.state.selectedFolderId.value = folderId
@@ -30,6 +35,41 @@ export class FolderSelectionActions {
 
   clearMainSelection(): void {
     this.state.selectedMainFolderId.value = null
+  }
+
+    async navigateBack(): Promise<void> {
+    if (this.state.currentHistoryIndex.value > 0) {
+      this.state.currentHistoryIndex.value--
+      const folderId = this.state.navigationHistory.value[this.state.currentHistoryIndex.value]
+      if (folderId) {
+        await this.selectFolder(folderId, true) 
+      }
+    }
+  }
+
+  async navigateForward(): Promise<void> {
+    if (this.state.currentHistoryIndex.value < this.state.navigationHistory.value.length - 1) {
+      this.state.currentHistoryIndex.value++
+      const folderId = this.state.navigationHistory.value[this.state.currentHistoryIndex.value]
+      if (folderId) {
+        await this.selectFolder(folderId, true)
+      }
+    }
+  }
+
+  private addToHistory(folderId: string): void {
+    const lastFolderId = this.state.navigationHistory.value[this.state.currentHistoryIndex.value]
+    if (lastFolderId !== folderId) {
+      this.state.navigationHistory.value = this.state.navigationHistory.value.slice(
+        0,
+        this.state.currentHistoryIndex.value + 1,
+      )
+
+      this.state.navigationHistory.value.push(folderId)
+      this.state.currentHistoryIndex.value = this.state.navigationHistory.value.length - 1
+
+      this.navigationService.addToHistory(folderId)
+    }
   }
 
   private clearSearch(): void {
