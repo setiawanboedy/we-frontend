5cfd236c0f4cc1c33f0c644a894c5581a96dbac9
setiawanboedy <gonisetiawan0@gmail.com>
2025-08-27 23:21:41 +0800

    feat: add folderdto

---
+++ src/application/dto/FolderDto.ts
@@ -0,0 +1,13 @@
+export interface FolderDto {
+  id: string;
+  name: string;
+  path: string;
+  parentId: string | null;
+  size: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface FolderWithChildrenDto extends FolderDto {
+  children: FolderWithChildrenDto[];
+}
