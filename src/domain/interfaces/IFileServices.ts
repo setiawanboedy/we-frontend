0d70eb6c3e4431074c6969e9925acd75470ec924
setiawanboedy <gonisetiawan0@gmail.com>
2025-08-28 16:24:30 +0800

    feat: add interface fileservice

---
+++ src/domain/interfaces/IFileServices.ts
@@ -0,0 +1,14 @@
+import type {
+  FileDto,
+  FileWithContentDto,
+} from '@/application/dto/FileDto'
+
+export interface IFileSearchService {
+  searchFiles(query: string): Promise<FileDto[]>
+  isValidQuery(query: string): boolean
+}
+
+export interface IFileDataService {
+  loadFilesByFolder(folderId: string): Promise<FileDto[]>
+  loadFile(fileId: string): Promise<FileWithContentDto>
+}
